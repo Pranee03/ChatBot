f92264c752b6aaffa3e6f9b395f971289bc7181d
Pranee03 <101640840+Pranee03@users.noreply.github.com>
2023-07-08 20:43:16 +0530

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,22 @@
+import openai
+import gradio
+
+#Enter your API Key here
+
+openai.api_key = "####"
+
+messages = [{"role": "system", "content": "assistant"}]
+
+def CustomChatGPT(user_input):
+    messages.append({"role": "user", "content": user_input})
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = messages
+    )
+    ChatGPT_reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": ChatGPT_reply})
+    return ChatGPT_reply
+
+demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "Chatbot using OpenAI")
+
+demo.launch(share=True)
